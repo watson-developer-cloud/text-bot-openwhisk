d80d0287ed1981809c75ca7a58e725e628f4679c
Elizabeth Kelly <elizabeth.kelly2@ibm.com>
2017-07-20 20:17:35 -0400

    Added pipeline-DEPLOY.sh

---
+++ .bluemix/pipeline-DEPLOY.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+###############################################
+# Install dependencies
+###############################################
+
+echo 'Installing dependencies...'
+sudo apt-get -qq update 1>/dev/null
+sudo apt-get -qq install jq 1>/dev/null
+sudo apt-get -qq install figlet 1>/dev/null
+
+figlet 'Node.js'
+
+echo 'Installing nvm (Node.js Version Manager)...'
+npm config delete prefix
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.2/install.sh | bash > /dev/null 2>&1
+. ~/.nvm/nvm.sh
+
+echo 'Installing Node.js 7.9.0...'
+nvm install 7.9.0 1>/dev/null
+npm install --progress false --loglevel error 1>/dev/null
+
+figlet 'OpenWhisk CLI'
+mkdir ~/wsk
+curl https://openwhisk.ng.bluemix.net/cli/go/download/linux/amd64/wsk > ~/wsk/wsk
+chmod +x ~/wsk/wsk
+export PATH=$PATH:~/wsk
+
+################################################
+# Create Services
+################################################
+figlet 'Services'
