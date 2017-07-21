cec2bca1abc578bf9d9deb09a1ca0f2f1741a66f
Elizabeth Kelly <elizabeth.kelly2@ibm.com>
2017-07-21 15:44:20 -0400

    Updated pipeline-DEPLOY

--- .bluemix/pipeline-DEPLOY.sh
+++ .bluemix/pipeline-DEPLOY.sh
@@ -60,4 +60,29 @@ export NLU_USERNAME=`echo $NLU_CREDENTIALS | jq -r .username`
 export NLU_PASSWORD=`echo $NLU_CREDENTIALS | jq -r .password`
 export NLU_URL=`echo $NLU_CREDENTIALS | jq -r .url`
 
+# Create Watson Conversation Service                                                                                                                                                                            
+figlet -f small	'Conversation'
+cf create-service conversation free conversation-openwhisk
+cf create-service-key conversation-openwhisk conversation-key
+
+CONVERSATION_CREDENTIALS=`cf service-key conversation-openwhisk conversation-key | tail -n +2`
+export CONVERSATION_USERNAME=`echo $CONVERSATION_CREDENTIALS | jq -r .username`
+export CONVERSATION_PASSWORD=`echo $CONVERSATION_CREDENTIALS | jq -r .password`
+CONVERSATION_WORKSPACE=`cat workspace.json`
+CONVERSATION_WORKSPACE_INTENTS=`echo $CONVERSATION_WORKSPACE | jq -r .intents`
+CONVERSATION_WORKSPACE_ENTITIES=`echo $CONVERSATION_WORKSPACE | jq -r .entities`
+CONVERSATION_WORKSPACE_DIALOG_NODES=`echo $CONVERSATION_WORKSPACE | jq -r .dialog_nodes`
+export CONVERSATION_WORKSPACE_ID=`curl -H "Content-Type: application/json" -X POST \
+-u $CONVERSATION_USERNAME:$CONVERSATION_PASSWORD \
+-d "{\"name\":\"Sample\",\"intents\":$CONVERSATION_WORKSPACE_INTENTS,\"entities\":$CONVERSATION_WORKSPACE_ENTITIES,\"language\":\"en\",\"description\":\"The Watson Weather Bot\",\"dialog_nodes\":$CONVERSATION_WORKSPACE_DIALOG_NODES}"\"https://gateway.watsonplatform.net/conversation/api/v1/workspaces?version=2017-05-26" | jq -r .workspace_id`
+
+# Create Weather Insights service
+figlet -f small 'weatherinsights'
+cf create-service weatherinsights Free-v2 weatherinsights-openwhisk
+cf create-service-key weatherinsights-openwhisk weatherinsights-key
+
+WEATHER_CREDENTIALS=`cf service-key weatherinsights-openwhisk weatherinsights-key | tail -n +2`
+export WEATHER_USERNAME=`echo $WEATHER_CREDENTIALS | jq -r .username`
+export WEATHER_PASSWORD=`echo $WEATHER_CREDENTIALS | jq -r .password`
+export WEATHER_URL=`echo $WEATHER_CREDENTIALS | jq -r .url`
 
