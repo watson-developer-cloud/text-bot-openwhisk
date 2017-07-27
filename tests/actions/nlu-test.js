9ef6f740ee3b8df1ec7e582e47f78c76423ea597
Elizabeth Kelly <elizabeth.kelly2@ibm.com>
2017-07-27 17:07:04 -0400

    Added test for Natural Langauge Understanding

---
+++ tests/actions/nlu-test.js
@@ -0,0 +1,44 @@
+const action = require('../../actions/nlu');
+const assert = require('assert');
+const nock = require('nock');
+
+describe('[action] NLU', function () {
+
+  beforeEach(function() {
+    nock('https://gateway.watsonplatform.net:443')
+      .post('/natural-language-understanding/api/v1/analyze')
+      .query({
+        version:'2017-02-27'
+      })
+      .reply(200, {
+        entities: []
+      });
+  });
+
+  it('should throw error if credentials are missing', function () {
+    const params = {
+      conversation: { input: {} }
+    };
+    return action.main(params).then(function() {
+      assert.fail('Missing credentials error was not found');
+    }).catch(function(error) {
+      assert(error.message === 'Argument error: username and password are required unless use_unauthenticated is set');
+    });
+  });
+
+  it('should call nlu when parameters are right', function () {
+    const params = {
+      NLU_USERNAME: 'foo',
+      NLU_PASSWORD: 'bar',
+      conversation: {
+        input: {
+          text: 'Hello'
+        },
+        context: {},
+      },
+    };
+    return action.main(params).then(function() {
+      assert(true);
+    }).catch(assert.ifError);
+  });
+});
