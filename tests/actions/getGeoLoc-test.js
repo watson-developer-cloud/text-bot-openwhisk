dfa17b5d3523150fdd2ee6e96501d66e18b6c0c9
Elizabeth Kelly <elizabeth.kelly2@ibm.com>
2017-07-27 17:28:32 -0400

    Started testing for getGeoLoc

---
+++ tests/actions/getGeoLoc-test.js
@@ -0,0 +1,62 @@
+const action = require('../../actions/getGeoLoc');
+const assert = require('assert');
+const nock = require('nock');
+
+describe('[action] getGeoLoc', function () {
+
+  beforeEach(function() {
+    nock('https://twcservice.mybluemix.net')
+      .get('/api/weather/v3/location/search')
+      .query({
+        query: 'City name'
+      })
+      .reply(200, {
+        location: {}
+      });
+  });
+
+  it('should throw error if credentials are missing', function () {
+    const params = {
+      conversation: {
+        input: {},
+        context: {
+          city: {
+            name: '',
+            number_of_states: null,
+            alternate_name: '',
+            states: {}
+          }
+        }
+      }
+    };
+    return action.main(params).then(function() {
+      assert.fail('Missing credentials error was not found');
+    }).catch(function(error) {
+      assert(error.message === 'Argument error: username and password are required unless use_unauthenticated is set');
+    });
+  });
+
+  it('should call getGeoLoc when parameters are right', function () {
+    const params = {
+      WEATHER_USERNAME: 'foo',
+      WEATHER_PASSWORD: 'bar',
+      WEATHER_URL: 'baz',
+      conversation: {
+        input: {
+          text: 'Hello'
+        },
+        context: {
+          city: {
+            name: '',
+            number_of_states: null,
+            alternate_name: '',
+            states: {}
+          }
+        },
+      },
+    };
+    return action.main(params).then(function() {
+      assert(true);
+    }).catch(assert.ifError);
+  });
+});
