3db54b428b39d1da818eb8f7e6cc134a869670cc
Elizabeth Kelly <elizabeth.kelly2@ibm.com>
2017-08-01 11:20:46 -0400

    :art: Linted cloudant actions

--- actions/cloudant-add.js
+++ actions/cloudant-add.js
@@ -1,51 +1,42 @@
-console.log('reading from cloudant');
 function main(params) {
   if (params._rev !== null) {
-    console.log('Document exists');
     var output = Object.assign({}, {conversation: params.conversation}, {_id: params._id}, {_rev: params._rev});
     return output;
   }
   else if (!params._id || !params.id) {
     Promise.reject(new Error('id cannot be null'));
   }
-    
+
   //load the package
   const Cloudant = require('cloudant');
   const username = params.CLOUDANT_USERNAME;
   const password = params.CLOUDANT_PASSWORD;
   var dbname = 'owtextbotdb';
   var owdb = null;
-  console.log('connecting to cloudant');
   //connect to Cloudant
   const cloudant = Cloudant({
     account: username,
     password: password
   });
-  console.log('connected');
-    
-  console.log('checking if exists');
+
   try {
-    console.log('creating');
     owdb = cloudant.db.create(dbname);
-    if (db != null) {
-      console.log('db exists');
+    if (owdb != null) {
       owdb = cloudant.db.use(dbname);
     }
   } catch(e) {
-    console.log('catching');    
     owdb = cloudant.db.use(dbname);
   }
-    
+
   return new Promise(function(resolve, reject) {
-    console.log('inserting');
-    owdb.insert({'context': {}}, params._id, function(err, body, header) {
+    owdb.insert({'context': {}}, params._id, function(err, body) {
       if (err) {
-        console.log('[db.insert]', err.message);
         return reject(err);
       }
-      console.log('You have inserted the doc.');
-      var output = Object.assign({}, {conversation: params.conversation}, {_id: params._id}, {_rev: body.rev});            
+      var output = Object.assign({}, {conversation: params.conversation}, {_id: params._id}, {_rev: body.rev});
       return resolve(output);
     });
   });
 }
+
+module.exports.main = main;
--- actions/cloudant-read.js
+++ actions/cloudant-read.js
@@ -1,4 +1,3 @@
-console.log('reading from cloudant');
 function main(params) {
   if (!params._id) {
     Promise.reject(new Error('id cannot be null'));
@@ -6,7 +5,7 @@ function main(params) {
   const Cloudant = require('cloudant');
   const username = params.CLOUDANT_USERNAME;
   const password = params.CLOUDANT_PASSWORD;
-    
+
   var dbname = 'owtextbotdb';
   var owdb = null;
   //connect to Cloudant
@@ -14,33 +13,27 @@ function main(params) {
     account: username,
     password: password
   });
-    
-  console.log('checking if exists');
+
   try {
-    console.log('creating');
     owdb = cloudant.db.create(dbname);
-    if (db != null) {
-      console.log('db exists');
+    if (owdb != null) {
       owdb = cloudant.db.use(dbname);
     }
   } catch(e) {
-    console.log('catching');    
     owdb = cloudant.db.use(dbname);
   }
-    
+
   return new Promise(function(resolve, reject) {
-    console.log('getting');
-    console.log(params.id);
     owdb.get(params._id, function(err, body) {
       if (err) {
-        console.log('get doc ', err.message);
         return reject(err);
       }
-      console.log('Got doc');
       var output = Object.assign({}, {conversation: params.conversation}, {_id: params._id}, {_rev: params._rev});
       output.conversation.context = body.context;
-            
+
       return resolve(output);
     });
   });
 }
+
+module.exports.main = main;
