d94fee834ac102e0de776dc3d11559a71cd1064f
Elizabeth Kelly <elizabeth.kelly2@ibm.com>
2017-08-01 11:06:57 -0400

    Edited for testing and linted

--- actions/conversation-weather.js
+++ actions/conversation-weather.js
@@ -1,46 +1,46 @@
-/**                                                                                                                                   
- * Calls the Conversation service and returns a conversation context.                                                                 
- * @param {Object} params The parameters                                                                                              
- * @param {String} params.CONVERSATION_USERNAME The username for the Conversation service.                                            
- * @param {String} params.CONVERSATION_PASSWORD The password for the Conversation service.                                            
+/**
+ * Calls the Conversation service and returns a conversation context.
+ * @param {Object} params The parameters
+ * @param {String} params.CONVERSATION_USERNAME The username for the Conversation service.
+ * @param {String} params.CONVERSATION_PASSWORD The password for the Conversation service.
  */
-console.log('starting conversation');
+const assert = require('assert');
+const watson = require('watson-developer-cloud');
+
 function main(params) {
-  console.log('calling conversation');
-  var context = params.conversation.context;
-  if (!context.weather_conditions && !context.system.branch_exited) {
-    console.log('returning params');
-    var output = params._id ? Object.assign({}, {conversation: params.conversation}, {_id: params._id}, {_rev: params._rev}) : Object.assign({}, {conversation: params.conversation});
-    return output;
-  }
   return new Promise(function(resolve, reject) {
-    const watson = require('watson-developer-cloud');
-    const USERNAME = params.CONVERSATION_USERNAME;
-    const PASSWORD = params.CONVERSATION_PASSWORD;
-    const WORKSPACE_ID = params.WORKSPACE_ID;
+    assert(params, 'params can not be null');
+    assert(params.CONVERSATION_USERNAME, 'params.CONVERSATION_USERNAME can not be null');
+    assert(params.CONVERSATION_PASSWORD, 'params.CONVERSATION_PASSWORD can not be null');
+    assert(params.conversation && params.conversation.context, 'params.conversation.context can not be null');
 
-    const conversation = watson.conversation({
-      username: USERNAME,
-      password: PASSWORD,
-      version: 'v1',
-      version_date: '2017-05-26'
-    });
+    var context = (params.conversation && params.conversation.context) ? params.conversation.context : {};
 
-    var city_name = params.conversation.context.city.name;
-    var context = (params.conversation && params.conversation.context ? params.conversation.context : {});
-        
-    conversation.message({
-      workspace_id: WORKSPACE_ID,
-      input: params.conversation.input,
-      context: context
-    }, function(err, response) {
-      if (err) {
-        return reject(err);
-      }
-      console.log('no error');
+    if (!context.weather_conditions && !context.system.branch_exited) {
       var output = params._id ? Object.assign({}, {conversation: params.conversation}, {_id: params._id}, {_rev: params._rev}) : Object.assign({}, {conversation: params.conversation});
-      output.conversation = response;
-      return resolve(output);
-    });
+      resolve(output);
+    } else {
+      const conversation = watson.conversation({
+        username: params.CONVERSATION_USERNAME,
+        password: params.CONVERSATION_PASSWORD,
+        version: 'v1',
+        version_date: '2017-05-26'
+      });
+
+      conversation.message({
+        workspace_id: params.WORKSPACE_ID,
+        input: params.conversation.input,
+        context: context
+      }, function(err, response) {
+        if (err) {
+          return reject(err);
+        }
+        var output = params._id ? Object.assign({}, {conversation: params.conversation}, {_id: params._id}, {_rev: params._rev}) : Object.assign({}, {conversation: params.conversation});
+        output.conversation = response;
+        return resolve(output);
+      });
+    }
   });
 }
+
+module.exports.main = main;
