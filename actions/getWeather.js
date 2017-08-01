7d3062a384488cf5264ac3cf65b2a5f6f8ed21ba
Elizabeth Kelly <elizabeth.kelly2@ibm.com>
2017-08-01 09:07:45 -0400

    Linting getWeather

--- actions/getWeather.js
+++ actions/getWeather.js
@@ -5,12 +5,26 @@
  * @param {Object} params.conversation.context.city.states[state].latitude The latitude of the city.
  * @param {String} params.conversation.context.city.states[state].longitude The longitude of the city.
  */
+const assert = require('assert');
+
 function main(params) {
-  if (!params || params.conversation.context.city.number_of_states !== 1 || params.conversation.context.weather_conditions) {
-    var output = params._id ? Object.assign({}, {conversation: params.conversation}, {_id: params._id}, {_rev: params._rev}) : Object.assign({}, {conversation: params.conversation});
-    return output;
-  } else {
-    return new Promise(function(resolve, reject) {
+  return new Promise(function(resolve, reject) {
+    assert(params, 'params can not be null');
+    assert(params.WEATHER_USERNAME, 'params.WEATHER_USERNAME can not be null');
+    assert(params.WEATHER_PASSWORD, 'params.WEATHER_PASSWORD can not be null');
+    assert(params.conversation.context, 'params.conversation.context can not be null');
+    assert(params.conversation.context.city, 'params.conversation.context.city can not be null');
+
+    if (!params || params.conversation.context.city.number_of_states !== 1 || params.conversation.context.weather_conditions) {
+      var output = params._id
+        ? Object.assign({}, {
+          conversation: params.conversation
+        }, {
+          _id: params._id
+        }, {_rev: params._rev})
+        : Object.assign({}, {conversation: params.conversation});
+      resolve(output);
+    } else {
       const request = require('request');
 
       const USERNAME = params.WEATHER_USERNAME;
@@ -40,7 +54,7 @@ function main(params) {
           units: 'e',
           language: 'en-US'
         }
-      }, function (error, response, body) {
+      }, function(error, response, body) {
         var weather_conditions = {};
 
         for (var i = 0; i < body.forecasts.length; i++) {
@@ -67,8 +81,7 @@ function main(params) {
                 phrase_32char: forecastDetail.night.phrase_32char
               }
             };
-          }
-          else {
+          } else {
             weather_conditions[forecastDetail.dow] = {
               night: {
                 temp: forecastDetail.night.temp,
@@ -86,14 +99,21 @@ function main(params) {
         if (error || response.statusCode != 200) {
           reject(error);
         } else {
-          var output = params._id ? Object.assign({}, {conversation: params.conversation}, {_id: params._id}, {_rev: params._rev}) : Object.assign({}, {conversation: params.conversation});
+          var output = params._id
+            ? Object.assign({}, {
+              conversation: params.conversation
+            }, {
+              _id: params._id
+            }, {_rev: params._rev})
+            : Object.assign({}, {conversation: params.conversation});
           if (!params.conversation.context.hasOwnProperty('weather_conditions')) {
-            console.log('adding weather property');
             output.conversation.context.weather_conditions = weather_conditions;
           }
           resolve(output);
         }
       });
-    });
-  }
+    }
+  });
 }
+
+module.exports.main = main;
